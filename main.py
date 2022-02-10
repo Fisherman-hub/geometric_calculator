2425862bc3b8cb93d6d8ba2a342c9231f43e8e31
ZhenyaRybakov <fishrybakov@gmail.com>
2022-02-10 22:02:01 +0300

    Прописаны методы площадей и объемов в классах фигур.

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 from tkinter import Tk, Text, BOTH, X, N, LEFT, RIGHT, Listbox
 from tkinter.ttk import Frame, Label, Entry, Button
 
+PI = 3,14
 
 class Figure:
-    pass
+    def __init__(self):
+        pass
 
+    def square(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+    def volume(self):
+        pass
+
+    def draw_figure(self):
+        pass
 
 class Circle(Figure):
     title = "Круг"
+
+    def __init__(self, radius):
+        super().__init__()
+        print("I am borning")
+        self.radius = radius
+
+    def area(self):
+        return PI*self.radius**2
+
+    def perimeter(self):
+        return 2*PI*self.radius
 
     @staticmethod
     def draw_figure():
         return 'Draw Circle'
 
 
-class Square:
+class Square(Figure):
     title = "Квадрат"
+
+    def __init__(self, x):
+        super().__init__()
+        self.x = x
+
+    def area(self):
+        return self.x**2
+
+    def perimeter(self):
+        return 4*self.x
 
     @staticmethod
     def draw_figure():
@@ -23,14 +57,33 @@ class Square:
         pass
 
 
-class Rectangle:
+class Rectangle(Square):
     title = "Прямоугольник"
-    pass
 
+    def __init__(self, x, y):
+        super().__init__(x)
+        self.y = y
 
-class Triangle:
+    def area(self):
+        return self.x * self.y
+
+    def perimeter(self):
+        return 2*(self.x + self.y)
+
+class Triangle(Rectangle):
     title = "Треугольник"
-    pass
+
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
+    def perimeter(self):
+        return self.x + self.y + self.z
+
+    def area(self):
+        p = self.perimeter()
+        return (p*(p-self.x)*(p-self.y)*(p-self.z))**0.5
+
 
 
 class Trapezoid:
@@ -38,34 +91,87 @@ class Trapezoid:
     pass
 
 
-class Rhombus:
+class Rhombus(Rectangle):
     title = "Ромб"
-    pass
 
+    def __init__(self, x, y):
+        super().__init__(x, y)
 
-class Sphere:
+    def area(self):
+        return self.x*self.y
+
+    def perimeter(self):
+        return 4*self.x
+
+class Sphere(Circle):
+
+    def __init__(self, radius):
+        super().__init__(radius)
+
+    def area(self):
+        return 4*PI*self.radius
+
+    def volume(self):
+        return (4/3)*self.radius**3
+
     title = "Сфера"
     pass
 
 
-class Cube:
+class Cube(Square):
     title = "Куб"
-    pass
+
+    def __init__(self):
+        super().__init__(x)
+
+    def area(self):
+        return 6*self.x
 
 
-class Parallelepiped:
+    def perimeter(self):
+        return 12*self.x
+
+    def volume(self):
+        return self.x**3
+
+class Parallelepiped(Rectangle):
     title = "Параллелепипед"
-    pass
+
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
+    def area(self):
+        return 2*(self.x*self.y + self.x*self.z + self.y*self.z)
+
+    def volume(self):
+        return self.x*self.y*self.z
+
+    def perimeter(self):
+        return 4*(self.x+self.y+self.z)
 
 
-class Pyramid:
+class Pyramid(Parallelepiped):
     title = "Пирамида"
-    pass
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
+    def volume(self):
+        return (1/3)*self.x*self.y*self.z
+
+    def area(self):
+        return self.x*self.y + self.x * self.z + self.y*self.z
 
 
-class Cylinder:
+class Cylinder (Rectangle):
     title = "Цилиндр"
-    pass
+
+    def __init__(self, x, y):
+        super().__init__(x, y)
+
+    def area(self):
+        return 2*PI*self.x*self.y + 2*
 
 
 class Cone:
@@ -86,18 +192,18 @@ class Example(Frame):
         frame1 = Frame(self)
         frame1.pack(fill=X)
 
-        figures = {"Круг": Circle.draw_figure(),
-                              "Квадрат": "",
-                              "Прямоугольник": "",
-                              "Треугольник": "",
-                              "Трапеция": "",
-                              "Ромб": "",
-                              "Сфера": "",
-                              "Куб": "",
-                              "Параллелепипед": "",
-                              "Пирамида": "",
-                              "Цилиндр": "",
-                              "Конус": ""}
+        figures = {"Круг": Circle(),
+                              "Квадрат": Square(),
+                              "Прямоугольник": Rectangle(),
+                              "Треугольник": Triangle(),
+                              "Трапеция": Trapezoid(),
+                              "Ромб": Rhombus(),
+                              "Сфера": Sphere(),
+                              "Куб": Cube(),
+                              "Параллелепипед": Parallelepiped(),
+                              "Пирамида": Pyramid(),
+                              "Цилиндр": Cylinder(),
+                              "Конус": Cone()}
 
         lbl1 = Label(frame1, text="Выберите фигуру", width=20)
         lbl1.pack(side=LEFT, padx=5, pady=5)
@@ -127,7 +233,8 @@ class Example(Frame):
         def proverka():
             print(list_figures.get(int(list_figures.curselection()[0])))
             a = list_figures.get(int(list_figures.curselection()[0]))
-            print(figures[a])
+            figure = figures[a]
+            print(figure.draw_figure())
 
         calculate_button.config(command=proverka)
 
